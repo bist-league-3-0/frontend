e14589111414e361d27dcaf7fda19087b53bcb35
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2020-07-04 18:53:24 +0700

    added react logic for login

--- src/scenes/Login/loginScene.js
+++ src/scenes/Login/loginScene.js
@@ -1,24 +1,58 @@
-import React, {Component} from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
-class LoginScene extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Login Form</h1>
-                <form action="http://localhost:9000/api/bistleague3/auth/login" method="post">
-                    <div>
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" name="email" id="email"/>
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password:</label>
-                        <input type="password" name="password" id="password"/>
-                    </div>
-                    <input type="submit" value="go on"/>
-                </form>
-            </div>
-        );
-    }
-}
+const LoginScene = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [verdict, setVerdict] = useState("");
+
+  const onLogin = async (event) => {
+    event.preventDefault();
+    await axios
+      .post("http://localhost:9000/api/bistleague3/auth/login", {
+        email,
+        password,
+      })
+      .then((response) => {
+        return;
+      })
+      .catch((err) => {
+        setVerdict(err.response.data.message);
+        return;
+      });
+  };
+
+  return (
+    <div>
+      <h1>Login Form</h1>
+      <form onSubmit={onLogin}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+        </div>
+        <input type="submit" value="go on" />
+      </form>
+      <div>{verdict}</div>
+    </div>
+  );
+};
 
 export default LoginScene;
