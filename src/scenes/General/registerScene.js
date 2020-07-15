851b9b9d0ac7fc3a933721ca2187c411088e80fd
HardyValen <hardyvalen@gmail.com>
2020-07-15 23:30:37 +0700

    Revamped Registration

--- src/scenes/General/registerScene.js
+++ src/scenes/General/registerScene.js
@@ -5,50 +5,45 @@ import { NavLink } from "react-router-dom";
 import FrontendRoutes from "../../routes/frontendRoutes";
 
 const RegisterScene = (props) => {
-  const [username, setUsername] = useState("");
+  const [teamname, setTeamname] = useState("");
+  const [institution, setInstitution] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [leadername, setLeadername] = useState("");
+  const [gender, setGender] = useState("Male");
+  const [major, setMajor] = useState("");
+  const [interest, setInterest] = useState("");
+  const [enrollmentyear, setEnrollmentyear] = useState(2020);
+  const [age, setAge] = useState(18);
   const [phone, setPhone] = useState("");
   const [line, setLine] = useState("");
-  const [institution, setInstitution] = useState("");
-  const [major, setMajor] = useState("");
-  const [gender, setGender] = useState("");
+  const [linkedin, setLinkedin] = useState("http://linkedin.com/in/");
+
   const [verdict, setVerdict] = useState({status: "", message:""});
 
-  const checkFormInput = () => {
-    // Check Password Length
-    if (password.length < 10) {
-      setVerdict({status: "error", message:"Your password should be of minimum 10 characters or more"})
-      return false;
-    }
-
-    return true;
-  }
-
-  const onRegister = (event) => {
-    event.preeventDefault();
+  const onRegister = async (event) => {
+    event.preventDefault();
     let endpoint = BackendRoutes.register;
 
-    if (checkFormInput()) {
-      console.log("Error")
-      // await axios
-      // .post(endpoint, {username, email, password, phone, line, institution, major, gender}, {withCredentials: true})
-      // .then(() => {
-      //   setVerdict({
-      //     status: "success", 
-      //     message: "Successfully Logged In, Please Wait"
-      //   });
-      //   window.location.replace("/");
-      //   return;
-      // })
-      // .catch(err => {
-      //   setVerdict({
-      //     status: "error", 
-      //     message: err.response.data.message
-      //   });
-      //   return;
-      // })
-    }
+    await axios
+      .post(endpoint, {teamname, leadername, email, password, phone, line, institution, major, gender, interest, enrollmentyear, age, linkedin}, {withCredentials: true})
+      .then((res) => {
+        setVerdict({
+          status: "success",
+          message: res.data.message
+        })
+        window.scrollTo(0, 0);
+        window.location.replace(FrontendRoutes.login);
+        return;
+      })
+      .catch((e) => {
+        setVerdict({
+          status: "error",
+          message: e.response.data.message
+        });
+        window.scrollTo(0, 0);
+        return;
+      })
   }
 
   useEffect(
@@ -66,58 +61,26 @@ const RegisterScene = (props) => {
 
         <form onSubmit={onRegister} className="form">
           <div className="flash-message" status={verdict.status}>
-            {verdict.mesage}
+            {verdict.message}
           </div>
           <span className="form-title">Create Team Account</span>
           <div className="input-body">
 
             <div className="input-group">
-              <label htmlFor="username" className="input-label">Team Name</label>
-              <input type="text" name="username" id="username" 
-                onChange={(event) => {
-                  setUsername(event.target.value);
-                }}
-              />
-            </div>
-
-            <div className="input-group">
-              <label htmlFor="email" className="input-label">Email</label>
-              <input type="email" name="email" id="email" 
-                onChange={(event) => {
-                  setEmail(event.target.value);
-                }}
-              />
-            </div>
-
-            <div className="input-group">
-              <label htmlFor="password" className="input-label">Password</label>
-              <input type="password" name="password" id="password" 
-                onChange={(event) => {
-                  setPassword(event.target.value);
-                }}
-              />
-            </div>
-
-            <div className="input-group">
-              <label htmlFor="phone-number" className="input-label">Phone Number</label>
-              <span className="input-text">
-                Please fill this with your active number.
+              <span className="input-heading">
+                Team Account
               </span>
-              <input type="text" name="phone-number" id="phone-number"
-                onChange={(event) => {
-                  setPhone(event.target.value);
-                }}
-              />
+              <hr/>
             </div>
-
+            
             <div className="input-group">
-              <label htmlFor="line-id" className="input-label">Line ID</label>
+              <label htmlFor="teamname" className="input-label">Team Name</label>
               <span className="input-text">
-                If you have a LINE Account, please insert your LINE account here
+                Team names that contain any offense towards a certain ethnicity, religion, or race are strictly prohibited.
               </span>
-              <input type="text" name="line-id" id="line-id"
+              <input type="text" name="teamname" id="teamname" required
                 onChange={(event) => {
-                  setLine(event.target.value);
+                  setTeamname(event.target.value);
                 }}
               />
             </div>
@@ -127,7 +90,7 @@ const RegisterScene = (props) => {
               <span className="input-text">
                 Please write your institution / school name without abbreviating its name and please write it in official name, ex: Institut Teknologi Bandung, not Bandung Institute of Technology
               </span>
-              <input type="text" name="institution" id="institution" 
+              <input type="text" name="institution" id="institution" required
                 onChange={(event) => {
                   setInstitution(event.target.value);
                 }}
@@ -135,10 +98,44 @@ const RegisterScene = (props) => {
             </div>
 
             <div className="input-group">
-              <label htmlFor="major" className="input-label">Major</label>
-              <input type="text" name="major" id="major" 
+              <label htmlFor="email" className="input-label">Email</label>
+              <span className="input-text">
+                Please fill this with your team leader email.
+              </span>
+              <input type="email" name="email" id="email" required
                 onChange={(event) => {
-                  setMajor(event.target.value);
+                  setEmail(event.target.value);
+                }}
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="password" className="input-label">Password</label>
+              <span className="input-text">
+                This password is for team account login. Password length should be 10 characters or more.
+              </span>
+              <input type="password" name="password" id="password" required
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }}
+              />
+            </div>
+
+            <div className="input-group">
+              <span className="input-heading">
+                Leader Info
+              </span>
+              <hr/>
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="leadername" className="input-label">Leader Name</label>
+              <span className="input-text">
+                Insert your full name here.
+              </span>
+              <input type="text" name="leadername" id="leadername" required
+                onChange={(event) => {
+                  setLeadername(event.target.value);
                 }}
               />
             </div>
@@ -147,7 +144,7 @@ const RegisterScene = (props) => {
               <label className="input-label">Gender</label>
               <div className="input-radio-wrapper">
                 <div className="input-radio">
-                  <input type="radio" name="gender" id="male" />
+                  <input type="radio" name="gender" id="male" value="Male" defaultChecked/>
                   <label htmlFor="male" 
                     onClick={() => setGender("Male")}
                     >
@@ -156,7 +153,7 @@ const RegisterScene = (props) => {
                   </label>
                 </div>
                 <div className="input-radio">
-                  <input type="radio" name="gender" id="female"/>
+                  <input type="radio" name="gender" id="female" value="Female"/>
                   <label htmlFor="female"
                     onClick={() => setGender("Female")}
                   >
@@ -165,7 +162,7 @@ const RegisterScene = (props) => {
                   </label>
                 </div>
                 <div className="input-radio">
-                  <input type="radio" name="gender" id="other"/>
+                  <input type="radio" name="gender" id="other" value="Prefer not to say"/>
                   <label htmlFor="other"
                     onClick={() => setGender("Prefer not to say")}
                   >
@@ -174,6 +171,90 @@ const RegisterScene = (props) => {
                   </label>
                 </div>
               </div>
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="major" className="input-label">Major</label>
+              <span className="input-text">
+                Please write your major without abbreviating its name and please write it in official name, ex: Teknik Informatika, not IT / IF
+              </span>
+              <input type="text" name="major" id="major" required
+                onChange={(event) => {
+                  setMajor(event.target.value);
+                }}
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="interest" className="input-label">Interests in Information Technology</label>
+              <span className="input-text">
+                Please write down your interests in information technology field, i.e. information security, artificial intelligence, UI/UX, etc.
+              </span>
+              <input type="text" name="interest" id="interest" required
+                onChange={(event) => {
+                  setInterest(event.target.value);
+                }}
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="enrollment" className="input-label">Year Of Enrollment</label>
+              <span className="input-text">
+                Please enter the year you got admitted at your current institution.
+              </span>
+              <input type="number" name="enrollment" id="enrollment" min="2012" max="2020" defaultValue="2020" required
+                onChange={(event) => {
+                  setEnrollmentyear(event.target.value);
+                }}
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="age" className="input-label">Age</label>
+              <span className="input-text">
+                Please insert your current age.
+              </span>
+              <input type="number" name="age" id="age" min="0" max="100" defaultValue="18" required
+                onChange={(event) => {
+                  setAge(event.target.value);
+                }}
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="phone" className="input-label">Phone Number</label>
+              <span className="input-text">
+                (Optional) Please fill this with your active number.
+              </span>
+              <input type="text" name="phone" id="phone" 
+                onChange={(event) => {
+                  setPhone(event.target.value);
+                }}
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="line" className="input-label">Line ID</label>
+              <span className="input-text">
+                (Optional) If you have a LINE Account, please insert your LINE account here
+              </span>
+              <input type="text" name="line" id="line"
+                onChange={(event) => {
+                  setLine(event.target.value);
+                }}
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="linkedin" className="input-label">LinkedIn</label>
+              <span className="input-text">
+                (Optional) Please enter a link to your LinkedIn profile, example: http://linkedin.com/in/linkedinyourname
+              </span>
+              <input type="text" name="linkedin" id="linkedin" defaultValue="http://linkedin.com/in/"
+                onChange={(event) => {
+                  setLinkedin(event.target.value);
+                }}
+              />
             </div>
 
           </div>
