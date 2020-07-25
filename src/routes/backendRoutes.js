40a411d4d0e7db754e3cd21e766cbf56149e5df6
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2020-07-26 02:33:43 +0700

    Hotfix trailing slash

--- src/routes/backendRoutes.js
+++ src/routes/backendRoutes.js
@@ -1,5 +1,5 @@
 const BackendRoutes = {};
-const BackendBase = "http://localhost:9000/"
+const BackendBase = process.env.REACT_APP_API_URL;
 const BackendURL = BackendBase + "api/bistleague3/";
 const BistAccount = BackendURL + "bist-account/";
 
@@ -17,21 +17,23 @@ BackendRoutes.checkAuth = BackendRoutes.login + "check-authentication/";
 BackendRoutes.getUser = BackendRoutes.auth + "users/";
 
 BackendRoutes.uploadFile = {
-  payment: BackendRoutes.home + "payment/upload/"
-}
+  payment: BackendRoutes.home + "payment/upload/",
+};
 
 BackendRoutes.bistAccount = {
-  changeAccountCredentials: BistAccount + "account-settings/change-account-credentials/",
+  changeAccountCredentials:
+    BistAccount + "account-settings/change-account-credentials/",
   changeTeamData: BistAccount + "team-management/change-team-data/",
   uploadPayment: BistAccount + "team-management/upload-payment/",
   changeMemberData: BistAccount + "member-management/change-member-data/",
-  uploadEnrollment: BistAccount + "member-management/upload-proof-of-enrollment/",
+  uploadEnrollment:
+    BistAccount + "member-management/upload-proof-of-enrollment/",
   uploadIDCard: BistAccount + "member-management/upload-student-id-card/",
   uploadPortrait: BistAccount + "member-management/upload-portrait-photo/",
   deleteMember: BistAccount + "member-management/delete-member/",
   addMember: BistAccount + "member-management/add-member/",
   uploadPreliminary: BistAccount + "submission/upload-preliminary/",
-  uploadFinal: BistAccount + "submission/upload-final/"
-}
+  uploadFinal: BistAccount + "submission/upload-final/",
+};
 
 module.exports = BackendRoutes;
