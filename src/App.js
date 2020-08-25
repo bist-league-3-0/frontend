537befb7f0fe199f25b0698a8b365a9957f5b2ae
HardyValen <hardyvalen@gmail.com>
2020-08-25 14:59:53 +0700

    fixed minor bug involved in navigation not showing when using direct links (2)

--- src/App.js
+++ src/App.js
@@ -45,27 +45,24 @@ const App = () => {
     <div>
       <Router>
         <ScrollToTop/>
+        <Component.Navigation width={width} height={height} user={user}/>
         <Switch>
           {/* General Routes */}
           <Route exact path={FrontendRoutes.home}>
             <Component.BISTHelmet title="Landing"/>
-            <Component.Navigation width={width} height={height} user={user}/>
             <Scene.LandingScene width={width} height={height}/>
             <Component.BISTFooter width={width} height={height}/>
           </Route>
           <Route path={FrontendRoutes.competition}>
             <Component.BISTHelmet title="Competition"/>
-            <Component.Navigation width={width} height={height} user={user}/>
             <Scene.CompetitionScene width={width} height={height}/>
             <Component.BISTFooter width={width} height={height}/>
           </Route>
           <Route path={FrontendRoutes.login}>
-            <Component.Navigation width={width} height={height} user={user}/>
             <Scene.LoginScene user={user}/>
           </Route>
           <Route path={FrontendRoutes.BISTalks}>
             <Component.BISTHelmet title="BISTalks"/>
-            <Component.Navigation width={width} height={height} user={user}/>
             <Switch>
               <Route exact path={FrontendRoutes.BISTalks1}>
                 <Scene.BISTalksScene width={width} height={height} version={1}/>
@@ -81,13 +78,11 @@ const App = () => {
           </Route>
           <Route path={FrontendRoutes.challenges}>
             <Component.BISTHelmet title="Hear Us Out"/>
-            <Component.Navigation width={width} height={height} user={user}/>
             <Scene.ChallengeScene width={width} height={height}/>
             <Component.BISTFooter width={width} height={height}/>
           </Route>
           <Route path={FrontendRoutes.webinar}>
             <Component.BISTHelmet title="Webinar"/>
-            <Component.Navigation width={width} height={height} user={user}/>
             <Switch>
               <Route exact path={FrontendRoutes.webinar1}>
                 <Scene.WebinarScene width={width} height={height} version={1}/>
@@ -103,15 +98,12 @@ const App = () => {
           </Route>
           <Route path={FrontendRoutes.register}>
             <Scene.RegisterScene />
-            <Component.Navigation width={width} height={height} user={user}/>
           </Route>
           <Route path={FrontendRoutes.forgotPasswordValidate}>
             <Scene.ForgotPasswordValidateScene />
-            <Component.Navigation width={width} height={height} user={user}/>
           </Route>
           <Route path={FrontendRoutes.forgotPassword}>
             <Scene.ForgotPasswordScene />
-            <Component.Navigation width={width} height={height} user={user}/>
           </Route>
 
           {/* Auth Routes */}
@@ -125,12 +117,10 @@ const App = () => {
           {/* Error routes */}
           <Route path={FrontendRoutes.forbidden}>
             <Component.BISTHelmet title="403 Forbidden"/>
-            <Component.Navigation width={width} height={height} user={user}/>
             <Scene.ErrorScene code="403"/>
           </Route>
           <Route>
-            <Component.BISTHelmet title="404 Not Found"/>
-            <Component.Navigation width={width} height={height} user={user}/>
+          <Component.BISTHelmet title="404 Not Found"/>
             <Scene.ErrorScene code="404"/>
           </Route>
         </Switch>
--- src/routes/frontendRoutes.js
+++ src/routes/frontendRoutes.js
@@ -6,16 +6,16 @@ FrontendRoutes.home = FrontendURL;
 FrontendRoutes.login = FrontendRoutes.home + "login/";
 FrontendRoutes.register = FrontendRoutes.home + "register/";
 FrontendRoutes.forgotPassword = FrontendRoutes.home + "forgot-password/";
-FrontendRoutes.forgotPasswordValidate = FrontendRoutes.home + "forgot-password/validate";
+FrontendRoutes.forgotPasswordValidate = FrontendRoutes.home + "forgot-password/validate/";
 FrontendRoutes.dashboard = FrontendRoutes.home + "dashboard/";
 FrontendRoutes.competition = FrontendRoutes.home + "competition/";
 FrontendRoutes.BISTalks = FrontendRoutes.home + "BISTalks/";
-FrontendRoutes.BISTalks1 = FrontendRoutes.BISTalks + "t1";
-FrontendRoutes.BISTalks2 = FrontendRoutes.BISTalks + "t2";
+FrontendRoutes.BISTalks1 = FrontendRoutes.BISTalks + "t1/";
+FrontendRoutes.BISTalks2 = FrontendRoutes.BISTalks + "t2/";
 FrontendRoutes.challenges = FrontendRoutes.home + "challenges/";
 FrontendRoutes.webinar = FrontendRoutes.home + "webinar/";
-FrontendRoutes.webinar1 = FrontendRoutes.webinar + "t1";
-FrontendRoutes.webinar2 = FrontendRoutes.webinar + "t2";
+FrontendRoutes.webinar1 = FrontendRoutes.webinar + "t1/";
+FrontendRoutes.webinar2 = FrontendRoutes.webinar + "t2/";
 
 // Error Routes
 FrontendRoutes.forbidden = FrontendRoutes.home + "forbidden/";
@@ -45,7 +45,7 @@ FrontendRoutes.dashRoutes.addMember = FrontendRoutes.dashRoutes.memberManagement
 
 // Routes to show navigation bar
 // App not animated? include route here!
-FrontendRoutes.showNav = [
+let showNav = [
   FrontendRoutes.home,
   FrontendRoutes.login,
   FrontendRoutes.register,
@@ -58,7 +58,10 @@ FrontendRoutes.showNav = [
   FrontendRoutes.webinar,
   FrontendRoutes.webinar1,
   FrontendRoutes.webinar2,
-  FrontendRoutes.challenges,
-]
+  FrontendRoutes.challenges
+];
+
+let showNavTrimmed = showNav.map(link => {return link.slice(0, -1)});
+FrontendRoutes.showNav = showNav.concat(showNavTrimmed);
 
 module.exports = FrontendRoutes;
